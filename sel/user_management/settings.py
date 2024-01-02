5f7c41c372888ec9f0ca007397f9bdfe1db8a362
SaikiranKorla <144864551+SaikiranKorla@users.noreply.github.com>
2024-01-02 10:30:37 -0500

    Update settings.py

--- sel/user_management/settings.py
+++ sel/user_management/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UserConfig',
-    'user_management.apps.UserManagementConfig',
     'social_django',
 ]
 
